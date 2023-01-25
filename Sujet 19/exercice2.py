93a0b2552db95b56063ad0497a302a1414aec2a1
KarloSins <92882549+KarloSins@users.noreply.github.com>
2023-01-25 20:57:23 +0100

    Update exercice2.py

--- Sujet 19/exercice2.py
+++ Sujet 19/exercice2.py
@@ -6,8 +6,7 @@ def cesar(message, decalage):
     resultat = ''
     for c in message:
         if 'A' <= c and c <= 'Z':
-            print(position_alphabet(c))
-            indice = (int(position_alphabet(c))+decalage) % 26
+            indice = (position_alphabet(c)+decalage) % 26
             resultat = resultat + ALPHABET[indice]
         else:
             resultat = resultat + c
