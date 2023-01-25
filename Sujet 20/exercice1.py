6330bd6c46c524481715e46855c5201570ba59e0
KarloSins <92882549+KarloSins@users.noreply.github.com>
2023-01-25 22:01:07 +0100

    Create exercice1.py

---
+++ Sujet 20/exercice1.py
@@ -0,0 +1,19 @@
+def ajoute_dictionnaires(d1:dict,d2:dict):
+    if d1 == {}:
+        return d2
+    elif d2 == {}:
+        return d1
+
+    d = dict()
+    for k1, v1 in d1.items():
+        for k2, v2 in d2.items():
+            if k1 == k2:
+                a = d1[k1] + d2[k2]
+                d[k1] = a
+            else:
+                if k1 not in d or k2 not in d:
+                    d[k1] = d1[k1]
+                    d[k2] = d2[k2]
+
+    return d
+  
