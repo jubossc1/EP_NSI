80ad4bf8d014990a50d6ee353f2588204ae6a8a0
jubossc1 <92919133+jubossc1@users.noreply.github.com>
2023-01-26 11:24:30 +0100

    Create exercice2.py

---
+++ Sujet 13/exercice2.py
@@ -0,0 +1,13 @@
+pieces = [1, 2, 5, 10, 20, 50, 100, 200]
+
+def rendu_monnaie(somme_due, somme_versee):
+    rendu = []
+    a_rendre = somme_versee - somme_due
+    i = len(pieces) - 1
+    while i > 0:
+        if pieces[i] <= a_rendre:
+            rendu.append(pieces[i])
+            a_rendre = a_rendre - pieces[i]
+        else:
+            i = i - 1
+    return rendu
