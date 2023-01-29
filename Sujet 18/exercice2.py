f66756de375c23b3169ecdf78461550eb796979d
jubossc1 <92919133+jubossc1@users.noreply.github.com>
2023-01-29 17:55:33 +0100

    Create exercice2.py

---
+++ Sujet 18/exercice2.py
@@ -0,0 +1,28 @@
+def est_un_ordre(tab):
+    '''
+    Renvoie True si tab est de longueur n et contient tous les
+    entiers de 1 à n, False sinon
+    '''
+    for i in range(1,len(tab) + 1):
+        if not i in tab:
+            return False
+    return True
+
+def nombre_points_rupture(ordre):
+    '''
+    Renvoie le nombre de point de rupture de ordre qui représente
+    un ordre de gènes de chromosome
+    '''
+    assert est_un_ordre(ordre) == True
+    n = len(ordre)
+    nb = 0
+    if ordre[0] != 1: # le premier n'est pas 1
+        nb = nb + 1
+    i = 0
+    while i < n - 1:
+        if ordre[i] - ordre[i+1] not in [-1, 1]: # l'écart n'est pas 1
+            nb = nb + 1
+        i = i + 1
+    if ordre[-1] != n: # le dernier n'est pas n
+        nb = nb + 1
+    return nb
