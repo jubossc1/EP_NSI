069028a0e36b69fc067047999d8af641457f1be6
jubossc1 <92919133+jubossc1@users.noreply.github.com>
2023-01-31 21:03:34 +0100

    Create exercice2.py

---
+++ Sujet 05/exercice2.py
@@ -0,0 +1,29 @@
+def nbLig(image):
+    '''renvoie le nombre de lignes de l'image'''
+    return len(image)
+
+def nbCol(image):
+    '''renvoie la largeur de l'image'''
+    return len(image[0])
+
+def negatif(image):
+    '''renvoie le négatif de l'image sous la forme d'une liste de listes'''
+    # on créé une image de 0 aux mêmes dimensions que le paramètre image
+    L = [[0 for k in range(nbCol(image))] for i in range(nbLig(image))]
+    for i in range(nbLig(image)):
+        for j in range(nbCol(image)):
+            L[i][j] = 255 - image[i][j]
+    return L
+
+def binaire(image, seuil):
+    '''renvoie une image binarisée de l'image sous la forme d'une liste de listes contenant des 0 si la valeur
+    du pixel est strictement inférieure au seuilet 1 sinon'''
+    # on crée une image de 0 aux mêmes dimensions que le paramètre image
+    L = [[0 for k in range(nbCol(image))] for i in range(nbLig(image))]
+    for i in range(nbLig(image)):
+        for j in range(nbCol(image)):
+            if image[i][j] < seuil :
+                L[i][j] = 0
+            else:
+                L[i][j] = 1
+    return L
