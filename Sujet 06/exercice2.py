f05652ac8d960b3528ed2ac432b50cb8f2ec4493
jubossc1 <92919133+jubossc1@users.noreply.github.com>
2023-01-31 21:14:14 +0100

    Create exercice2.py

---
+++ Sujet 06/exercice2.py
@@ -0,0 +1,15 @@
+from math import sqrt # import de la fonction racine carrée
+
+def distance(point1, point2):
+    """ Calcule et renvoie la distance entre deux points. """
+    return sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+def plus_courte_distance(tab, depart):
+    """ Renvoie le point du tableau tab se trouvant à la plus courte distance du point depart."""
+    point = tab[0]
+    min_dist = distance(point, depart)
+    for i in range (1, len(tab)):
+        if distance(tab[i], depart) < min_dist:
+            point = tab[i]
+            min_dist = distance(tab[i], depart)
+    return point
