4d61521484c3a6434562c43bb4693b2fced0fa58
KarloSins <92882549+KarloSins@users.noreply.github.com>
2023-02-02 15:12:26 +0100

    Update exercice1.py

--- Sujet 35/exercice1.py
+++ Sujet 35/exercice1.py
@@ -1,3 +1,13 @@
 def ou_exclusif(tab1,tab2):
     return [tab1[i] ^ tab2[i] for i in range(len(tab1))]
 
+
+#OU version plus longue/moins complquée
+
+def ou_exclusifV2(tab1,tab2):
+    resultat = []
+    for i in range(len(tab1)):
+        resultat.append(tab1[i]^tab2[i])
+      return resultat
+
+        
