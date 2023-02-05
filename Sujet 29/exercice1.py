d6fd9cd471c92cee0f5d28640bc2e8665d83c025
KarloSins <92882549+KarloSins@users.noreply.github.com>
2023-02-05 20:21:53 +0100

    Update exercice1.py

--- Sujet 29/exercice1.py
+++ Sujet 29/exercice1.py
@@ -1,1 +1,15 @@
+def taille(arbre):
+    if arbre is None:
+        return 0
+    else:
+        return 1 + taille(arbre.fg) + taille(arbre.fd)
+      
+      
+
+def hauteur(arbre):
+    if arbre is None:
+        return 0
+    else:
+        return 1 + max(hauteur(arbre.fg),hauteur(arbre.fd))
+
 
