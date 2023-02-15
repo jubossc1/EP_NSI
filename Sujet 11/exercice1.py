42ebd2eb7cfa65d4531e0a2d5cad9dbad79fcde3
KarloSins <92882549+KarloSins@users.noreply.github.com>
2023-02-15 10:59:50 +0100

    Update exercice1.py

--- Sujet 11/exercice1.py
+++ Sujet 11/exercice1.py
@@ -5,3 +5,11 @@ def convertir(tab):
         if tab[i] == 1:
             nb += 2 ** (l - i - 1)
     return nb
+
+def convertir(tab):
+    resultat = 0
+    indice = len(tab) - 1
+    for element in tab:
+        resultat += element * (2 ** indice)
+        indice -= 1
+    return resultat
