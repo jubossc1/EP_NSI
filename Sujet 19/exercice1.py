45793755f034e939ed4444fe400beca29c2feb6e
KarloSins <92882549+KarloSins@users.noreply.github.com>
2023-02-16 15:30:07 +0100

    Update exercice1.py

--- Sujet 19/exercice1.py
+++ Sujet 19/exercice1.py
@@ -1,29 +1,14 @@
-#Programme Faux
-def recherche(tab:list,n:int):
-    if n < tab[0] or n > tab[-1] or n not in tab:
-        return -1
-    m = len(tab)//2
-    while True:
-        if n > tab[m]:
-            m += 1
-        elif n <tab[m]:
-            m-=1
+def recherche(tab, x):
+    debut = 0
+    fin = len(tab) - 1
+    while debut <= fin:
+        m = (debut + fin)//2
+        if x == tab[m]:
+            return True
+        if x > tab[m]:
+            debut = m + 1
         else:
-            return m
-       
+             fin = m - 1
+    return False
 
-#Solution valable
-def recherche(tab:list, n:int):
-    if n not in tab:
-        return -1
-    min = 0
-    max = len(tab)
-    while True:
-        m = min + (max - min) // 2 + len(tab) % 2
-        if tab[m] > n:
-            max = m
-        elif tab[m] < n:
-            min = n
-        else:
-            return m
 
