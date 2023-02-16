c5f118cfa285d1a916a74e4dbad36aeba315e9e4
jubossc1 <92919133+jubossc1@users.noreply.github.com>
2023-02-16 15:11:28 +0100

    Update exercice1.py

--- Sujet 19/exercice1.py
+++ Sujet 19/exercice1.py
@@ -10,3 +10,20 @@ def recherche(tab:list,n:int):
             m-=1
         else:
             return m
+       
+
+#Solution valable
+def recherche(tab:list, n:int):
+    if n not in tab:
+        return -1
+    min = 0
+    max = len(tab)
+    while True:
+        m = min + (max - min) // 2 + len(tab) % 2
+        if tab[m] > n:
+            max = m
+        elif tab[m] < n:
+            min = n
+        else:
+            return m
+
