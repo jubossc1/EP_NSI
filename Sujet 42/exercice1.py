b7d4d85641ba0022416dbaed2fb60b78abba6ad7
jubossc1 <92919133+jubossc1@users.noreply.github.com>
2023-02-16 17:49:07 +0100

    Update exercice1.py

--- Sujet 42/exercice1.py
+++ Sujet 42/exercice1.py
@@ -1,1 +1,8 @@
-
+def tri_selection(tab:list):
+    for i in range(len(tab)):
+        min = i
+        for j in range(i, len(tab)):
+            if tab[j] < tab[min]:
+                min = j
+        tab[i], tab[min] = tab[min], tab[i]
+    return tab
