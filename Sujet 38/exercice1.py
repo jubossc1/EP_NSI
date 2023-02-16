7a91cf5f32c42de943b524c4e79ef552996715ff
jubossc1 <92919133+jubossc1@users.noreply.github.com>
2023-02-16 15:31:30 +0100

    Update exercice1.py

--- Sujet 38/exercice1.py
+++ Sujet 38/exercice1.py
@@ -1,12 +1,3 @@
-#Solution incorrect (par karl)
-def correspond(mot, mot_a_trous):
-    if len(mot_a_trous) == len(mot):
-        for i in range(len(mot)):
-            if mot_a_trous[i] == mot[i] or mot_a_trous[i]=="*":
-                return True
-    return False
-
-#Solution fonctionnel
 def correspond(mot:str, mot_a_trou:str):
     if len(mot) == len(mot_a_trou):
         for i in range(len(mot)):
